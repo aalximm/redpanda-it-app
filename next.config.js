e25591de6cf82f0150a7009ce09f6031b427db25
Viacheslav Kuznetsov <alxim192@gmail.com>
2023-03-05 17:46:49 +0800

    remove static import

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	exportPathMap: async function (
-		defaultPathMap,
-		{ dev, dir, outDir, distDir, buildId }
-	) {
-		return {
-			'/': { page: '/' },
-		}
-	},
+	// exportPathMap: async function (
+	// 	defaultPathMap,
+	// 	{ dev, dir, outDir, distDir, buildId }
+	// ) {
+	// 	return {
+	// 		'/': { page: '/' },
+	// 	}
+	// },
 	images: {
 		unoptimized: true,
 	},
